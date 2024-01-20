a0775cf53c0fa7609902fc20c9e757f76fe4fec5
SonjaLu <s_lueg@yahoo.com>
2024-01-20 14:11:13 +0100

    Add netlify.toml and update projects.js

--- frontend/src/screens/Portfolio/projects.js
+++ frontend/src/screens/Portfolio/projects.js
@@ -1,5 +1,47 @@
 export const projects = [
     {
+        title: "Tetris",
+        description: "Python-Pygame-Project",
+        image: Tetrisdisplay,
+        url: "https://github.com/SonjaLu/Tetris-Clone",
+    },
+    {
+        title: "Gourmet Explorer",
+        description: "React-Vite-MongoDB-Project",
+        image: FullStackProject,
+        url: "https://github.com/SonjaLu/React-MongoDB-Project",
+    },
+    {
+        title: "Travelblog",
+        description: "Javascript-JQuery-Project",
+        image: Travelblog,
+        url: "https://sl-travelblog.netlify.app/",
+    },
+    {
+        title: "Personnel Management",
+        description: "office tool in React, exciting layout",
+        image: PersonnelManagement,
+        url: "https://github.com/SonjaLu/PersonnelManagement",
+    },
+    {
+        title: "Pokedex",
+        description: "Javascript-API-Project",
+        image: Pokedex,
+        url: "",
+    },
+    {
+        title: "K Drama",
+        description: "Layout Project with css",
+        image: KDrama,
+        url: "https://github.com/SonjaLu/LittleLayoutAndResponsivePage",
+    },
+    {
+        title: "TicTacToe",
+        description: "simple javascript project",
+        image: Tictactoe,
+        url: "",
+    },
+    {
         title: "",
         description: "",
         image: "",
